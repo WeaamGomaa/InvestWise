08b770564faf168a3909b3581d12dce5c083b00c
nagatrehan <nagatmuhammadd@gmail.com>
2025-05-10 20:40:47 +0300

    Update UserService

--- src/UserService.java
+++ src/UserService.java
@@ -1,12 +1,18 @@
+import java.io.*;
+import java.util.List;
+import java.util.ArrayList;
+
 public class UserService {
     private static final String FILENAME = "users_list.ser";
     private List<User> users = new ArrayList<>();
 
     public UserService() {
-        List<User> loadedUsers;
         try (FileInputStream fis = new FileInputStream(FILENAME);
              ObjectInputStream ois = new ObjectInputStream(fis)) {
-            loadedUsers = (List<UltraSimpleUserStorage.User>) ois.readObject(); // Deserialize
+            users = (List<User>) ois.readObject(); // Deserialize
+        }
+        catch (Exception e) {
+            System.out.println("Error loading users: " + e.getMessage());
         }
     }
 
@@ -20,9 +26,28 @@ public class UserService {
     }
 
     public void registerUser(String name, String email, String username, String password) {
+        if (getUser(username) != null) {
+            System.out.println("Username already exists!");
+            return;
+        }
 
+        users.add(User.register(name, email, username, password));
     }
+
+    public User authenticate(String username, String password) {
+        User user = getUser(username);
+        if (user == null) {
+            System.out.println("User not found!");
+            return null;
+        }
+        return (user.login(password)) ? user : null;
+    }
+
 }
+
+
+
+
 
 
 
