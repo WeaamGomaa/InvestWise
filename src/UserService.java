059fb53a66dc2a3837770242bc35502d079e0892
nagatrehan <nagatmuhammadd@gmail.com>
2025-05-10 20:34:54 +0300

    Update UserService

--- src/UserService.java
+++ src/UserService.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
-
 public class UserService {
+    private static final String FILENAME = "users_list.ser";
+    private List<User> users = new ArrayList<>();
+
+    public UserService() {
+        List<User> loadedUsers;
+        try (FileInputStream fis = new FileInputStream(FILENAME);
+             ObjectInputStream ois = new ObjectInputStream(fis)) {
+            loadedUsers = (List<UltraSimpleUserStorage.User>) ois.readObject(); // Deserialize
+        }
+    }
+
+    private User getUser(String username) {
+        for (User user : users) {
+            if (user.getUsername().equals(username)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public void registerUser(String name, String email, String username, String password) {
+
+    }
 }
+
+
+
+
+
+
+
