634ff9d04568aa0d6c4d9acd3a894ff4443501e9
nagatrehan <161480842+nagatrehan@users.noreply.github.com>
2025-05-13 12:24:18 +0300

    UserService updated

--- src/UserService.java
+++ src/UserService.java
@@ -1,18 +1,31 @@
+package main.java.investWise;
+
 import java.io.*;
 import java.util.List;
 import java.util.ArrayList;
 
+
 public class UserService {
     private static final String FILENAME = "users_list.ser";
+    private static final String DATA_FILE = "users.dat";
     private List<User> users = new ArrayList<>();
 
     public UserService() {
         try (FileInputStream fis = new FileInputStream(FILENAME);
              ObjectInputStream ois = new ObjectInputStream(fis)) {
-            users = (List<User>) ois.readObject(); // Deserialize
+            users = (List<User>) ois.readObject();
         }
         catch (Exception e) {
             System.out.println("Error loading users: " + e.getMessage());
+        }
+    }
+
+    public void saveUsers() {
+        try (FileOutputStream fos = new FileOutputStream(DATA_FILE);
+             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+            oos.writeObject(this.users);
+        } catch (IOException e) {
+            System.out.println("Error saving users: " + e.getMessage());
         }
     }
 
@@ -43,15 +56,7 @@ public class UserService {
         return (user.login(password)) ? user : null;
     }
 
+    public List<User> getAllUsers() {
+        return new ArrayList<>(users);
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
