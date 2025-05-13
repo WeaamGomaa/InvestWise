d45ef1ee9fbb0d6c3ee1d0b6fd73544275298909
nagatrehan <161480842+nagatrehan@users.noreply.github.com>
2025-05-13 13:41:53 +0300

    main updated

--- src/main/java/investWise/Main.java
+++ src/main/java/investWise/Main.java
@@ -1,17 +1,8 @@
 package main.java.investWise;
 
-//import main.java.investWise.assets.Asset;
-//import main.java.investWise.assets.AssetManager;
-//import main.java.investWise.observers.Observer;
-//import main.java.investWise.observers.Dashboard;
-//import main.java.investWise.MenuHandler;
-
-import main.java.investWise.assets.AssetManager;
-
 public class Main {
     public static void main(String[] args) {
-        AssetManager manager = new AssetManager();
         MenuHandler menuHandler = new MenuHandler();
-        menuHandler.showMainMenu(manager);
+        menuHandler.showLoginSignupMenu();
     }
 }
